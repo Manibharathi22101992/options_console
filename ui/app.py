0c6c756f02dd24b0ee55fcbcc4eb01ba604234db
Manibharathi22101992 <manibharathi.ct@gmail.com>
2026-08-06 22:20:52 +0530

    Update app.py

--- ui/app.py
+++ ui/app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Tell Python to look in the main folder for our other modules
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import streamlit as st
 import time
 from data.dhan_client import DhanMarketData
